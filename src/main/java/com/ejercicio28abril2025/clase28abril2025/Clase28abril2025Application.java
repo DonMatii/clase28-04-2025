c18918d8d394ab1e2c89e10031c75546f4217f01
Tu Nombre <tuemail@ejemplo.com>
2025-04-28 21:01:36 -0400

    Creacion Inicial

---
+++ src/main/java/com/ejercicio28abril2025/clase28abril2025/Clase28abril2025Application.java
@@ -0,0 +1,13 @@
+package com.ejercicio28abril2025.clase28abril2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase28abril2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase28abril2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejercicio28abril2025/clase28abril2025/Clase28abril2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio28abril2025.clase28abril2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase28abril2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
